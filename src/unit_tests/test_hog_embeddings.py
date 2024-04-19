3c0116da16819cb962dc5743b002fb486925a851
timoniche <dmitriidulaev@gmail.com>
2024-04-19 16:38:44 +0300

    unit test fix (sys issues, todo: change dir naming to file path further, change prints to log)

--- src/unit_tests/test_hog_embeddings.py
+++ src/unit_tests/test_hog_embeddings.py
@@ -1,21 +1,32 @@
+import os
 import pathlib
+import sys
 import unittest
 
 import numpy as np
 
-from images_dataset import hog_embedding
+sys.path.insert(1, os.path.join(os.getcwd(), "src"))
+
+from images_dataset import hog_embedding  # noqa: E402
 
 
 class TestHogEmbeddings(unittest.TestCase):
 
     def setUp(self) -> None:
-        images_dir = str(pathlib.Path().resolve().parent.parent)
+        images_dir = str(pathlib.Path(__file__).resolve().parent.parent.parent)
+        print('images dir ', images_dir)
         self.img_path = images_dir + '/client_images/' + '000001.jpg'
+
+        gt = '/Users/timoniche/Documents/BigData/mle-template/client_images/000001.jpg'
+        assert '/Users/timoniche/Documents/BigData/mle-template/client_images/000001.jpg' == self.img_path, f'{gt} and {self.img_path}'
+        print(self.img_path)
 
     def test_hog_embeddings(self):
         hog_embs = hog_embedding(self.img_path)
 
-        with open('./ground_truth/hog_embeddings.txt', 'r') as f:
+        cur_dir = str(pathlib.Path(__file__).resolve().parent)
+        print('EEEE ', cur_dir)
+        with open(f'{cur_dir}/ground_truth/hog_embeddings.txt', 'r') as f:
             embs = f.readline()
             ground_truth = list(map(float, embs.split()))
 
